acfb7e579159d1fabac474f06a04c594528b1908
Andrew Skiba <skibaa@gmail.com>
2014-01-19 00:02:37 +0200

    step 2 - hardway

--- iteratees/PlayIteratees/app/workshop/MyIteratees.scala
+++ iteratees/PlayIteratees/app/workshop/MyIteratees.scala
@@ -4,15 +4,23 @@ import play.api.libs.iteratee._
 
 object MyIteratees {
   
-  def consume( prevData:Seq[Int] ):Iteratee[Int, Seq[Int]] = Cont {
-    case Input.El( i ) => consume( prevData :+ i )
-    case Input.EOF => Done( prevData )
-    case Input.Empty => consume( prevData )
+  def fold[A] ( start:A )( folder: Function2[A, Int, A]):Iteratee[Int, A] = Cont {
+    case Input.El( i ) => fold( folder(start, i) )( folder )
+    case Input.EOF => Done( start )
+    case Input.Empty => fold( start )( folder )
   }
+  
+  def consume( prevData:Seq[Int] ):Iteratee[Int, Seq[Int]] = 
+    fold( prevData ) { (prev, i) =>
+	  prev :+ i
+	}
   
   val consumer = consume( Seq.empty )
   
-  def sum( prevSum:Int ):Iteratee[Int, Int] = Done( prevSum )
+  def sum( prevSum:Int ):Iteratee[Int, Int] = 
+  	fold ( prevSum ) { (prev, i) =>
+      prev + i
+    }
   
   val summer = sum( 0 )
 }
